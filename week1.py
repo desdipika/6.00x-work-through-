dd1bd5bcec73cb8ce4e49122df93537ed52e60ec
desdipika <35023867+desdipika@users.noreply.github.com>
2018-01-08 23:28:25 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -6,6 +6,20 @@ else:
   print("x is odd")
 print("Done with conditional") 
 
+# A nested conditional program to see if an int is divisible by 2 and 3
+if x%2 == 0:
+	if x%3 == 0:
+		print("x is divisible by 2 and 3")
+	else:
+		print("x is divisible by 2 and not 3")
+elif x%3 == 0:
+	print("x is divisible by 3 and not 2")
+else:
+	print("x not divisible by 2 and 3")
+print("Done with nested conditional")
+
+
+
   
   
  
