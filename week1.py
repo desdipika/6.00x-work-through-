f2470018bab216c2f79dff8c1d3c457ca2c221f0
desdipika <35023867+desdipika@users.noreply.github.com>
2018-01-08 23:10:23 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,1 +1,11 @@
+# A branching program to see if an int is even or odd
+x = int(input("Enter a number: "))
+if x%2 == 0:
+  print("x is even")
+else:
+  print("x is odd")
+print("Done with conditional") 
 
+  
+  
+ 
