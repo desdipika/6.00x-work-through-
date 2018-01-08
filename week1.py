c7cad8f27180bbec20633b3f298b0d1ca9e69ec4
desdipika <35023867+desdipika@users.noreply.github.com>
2018-01-09 00:42:23 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -18,6 +18,23 @@ else:
 	print("x not divisible by 2 and 3")
 print("Done with nested conditional")
 
+# A conditional to compare ints and print the least
+y = int(input("Enter another number: "))
+z = int(input("Enter one more: "))
+if x<y and x<z:
+	print("x is the least")
+elif y<z:
+	print("y is the least")
+else:
+	print("z is the least")
+# A conditional to compare ints and print the greatest
+if x>y and x>z:
+	print("x is the greatest")
+elif y>z:
+	print("y is the greatest")
+else:
+	print("z is the greatest")
+print("Done with comparison")
 
 
   
